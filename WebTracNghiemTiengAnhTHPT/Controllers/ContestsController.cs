cb26bb273ba58bcdf9db657b62cdc34b72c55400
hophuoc4so9 <2224802010872@student.tdmu.edu.vn>
2024-09-19 19:24:27 +0700

    database

--- WebTracNghiemTiengAnhTHPT/Controllers/ContestsController.cs
+++ WebTracNghiemTiengAnhTHPT/Controllers/ContestsController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebTracNghiemTiengAnhTHPT.Models;
 namespace WebTracNghiemTiengAnhTHPT.Controllers
 {
     public class ContestsController : Controller
@@ -11,7 +11,10 @@ namespace WebTracNghiemTiengAnhTHPT.Controllers
         // GET: Contests
         public ActionResult Index()
         {
-            return View();
+            TracNghiemTiengAnhTHPTEntities db = new TracNghiemTiengAnhTHPTEntities(); 
+            List<KyThi> model= db.KyThis.ToList();
+            return View(model);
+            
         }
     }
 }
