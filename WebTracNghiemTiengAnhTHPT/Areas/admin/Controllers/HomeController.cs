5a3d54125843e3feef6f96bc2d3960aed11a4536
Alien <minhhnghiii@gmail.com>
2024-09-19 18:39:49 +0700

    completed admin page layout

---
+++ WebTracNghiemTiengAnhTHPT/Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTracNghiemTiengAnhTHPT.Areas.admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult RenderNavbar()
+        {
+            return PartialView("_Navbar");
+        }
+    }
+}
---
+++ WebTracNghiemTiengAnhTHPT/Areas/admin/adminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace WebTracNghiemTiengAnhTHPT.Areas.admin
+{
+    public class adminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[ ] {"WebTracNghiemTiengAnhTHPT.Areas.admin.Controllers"} 
+            );
+        }
+    }
+}
