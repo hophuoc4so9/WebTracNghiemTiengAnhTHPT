e4a3da12644dc3b47bdf98ab8aab5a5e3e3edc7f
Alien <minhhnghiii@gmail.com>
2024-10-24 20:35:39 +0700

    Tao de ngau nhien theo lua chon

--- WebTracNghiemTiengAnhTHPT/Areas/giaovien/Controllers/QuanLyDeThiController.cs
+++ WebTracNghiemTiengAnhTHPT/Areas/giaovien/Controllers/QuanLyDeThiController.cs
@@ -407,6 +407,82 @@ namespace WebTracNghiemTiengAnhTHPT.Areas.giaovien.Controllers
                 }
             }
         }
+        public ActionResult AutoGenerateExam()
+        {
+            using (var db = new TracNghiemTiengAnhTHPTEntities1())
+            {
+                var totalQuestions = db.CauHois.Count();
+                var easyQuestions = db.CauHois.Count(q => q.MucDo == 1); 
+                var hardQuestions = db.CauHois.Count(q => q.MucDo == 2); 
+
+                ViewBag.TotalQuestions = totalQuestions;
+                ViewBag.EasyQuestions = easyQuestions;
+                ViewBag.HardQuestions = hardQuestions;
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AutoGenerateExam(int SoCauHoiDe, int SoCauHoiKho, int ThoiGian)
+        {
+            using (var db = new TracNghiemTiengAnhTHPTEntities1())
+            {
+                var easyQuestions = db.CauHois
+                    .Where(q => q.MucDo == 1 && !q.isDeleted)
+                    .ToList();
+                var hardQuestions = db.CauHois
+                    .Where(q => q.MucDo == 2 && !q.isDeleted)
+                    .ToList();
+
+                if (SoCauHoiDe > easyQuestions.Count || SoCauHoiKho > hardQuestions.Count)
+                {
+                    ModelState.AddModelError("", "Không đủ câu hỏi để tạo đề thi.");
+                    TempData["Error"] = "Không đủ câu hỏi để tạo đề thi.";
+                    ViewBag.TotalQuestions = db.CauHois.Count();
+                    ViewBag.EasyQuestions = easyQuestions.Count;
+                    ViewBag.HardQuestions = hardQuestions.Count;
+                    return View(); 
+                }
+
+                Random random = new Random();
+                var selectedEasyQuestions = easyQuestions
+                    .OrderBy(x => random.Next())
+                    .Take(SoCauHoiDe)
+                    .ToList();
+                var selectedHardQuestions = hardQuestions
+                    .OrderBy(x => random.Next())
+                    .Take(SoCauHoiKho)
+                    .ToList();
+
+                var newExam = new KyThi
+                {
+                    TenKyThi = "Đề Thi Ngẫu Nhiên " + DateTime.Now.ToString("yyyyMMddHHmm") + " - " + Guid.NewGuid().ToString().Substring(0, 8),
+                    ThoiGian = ThoiGian,
+                    ThoiGianBatDau = DateTime.Now,
+                    isDeleted = false,
+                    UsernameTacGia = Session["UserName"]?.ToString()
+                };
+
+                db.KyThis.Add(newExam);
+                db.SaveChanges();
+
+                foreach (var question in selectedEasyQuestions)
+                {
+                    newExam.CauHois.Add(question);
+                }
+                foreach (var question in selectedHardQuestions)
+                {
+                    newExam.CauHois.Add(question);
+                }
+
+                db.SaveChanges(); 
+
+                TempData["Success"] = "Đề thi đã được tạo thành công!";
+                return RedirectToAction("Index");
+            }
+        }
+
+
 
     }
 
